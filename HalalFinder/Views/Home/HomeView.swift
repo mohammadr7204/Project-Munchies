1625e800aa4ca87ca0cb52364330d2b280088d09
mohammadr7204 <mohammadr7204@gmail.com>
2025-01-21 23:39:33 -0500

    Implement basic HomeView with MapKit and layout structure

--- HalalFinder/Views/Home/HomeView.swift
+++ HalalFinder/Views/Home/HomeView.swift
@@ -1,1 +1,43 @@
-// Main map view with bottom sheet
+import SwiftUI
+import MapKit
+
+struct HomeView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194), // Default to SF
+        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+    )
+    
+    @State private var isBottomSheetExpanded = false
+    
+    var body: some View {
+        ZStack {
+            // Map View
+            Map(coordinateRegion: $region)
+                .edgesIgnoringSafeArea(.all)
+            
+            // Search Bar at top
+            VStack {
+                SearchBar()
+                    .padding(.horizontal)
+                    .padding(.top, 8)
+                
+                Spacer()
+            }
+            
+            // Bottom Sheet
+            VStack {
+                Spacer()
+                BottomSheet(isExpanded: $isBottomSheetExpanded) {
+                    RestaurantList()
+                }
+            }
+            .edgesIgnoringSafeArea(.bottom)
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
