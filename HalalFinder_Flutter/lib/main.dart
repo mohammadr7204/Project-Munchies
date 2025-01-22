a4070566725192cf5e67ab9e71c158b90c5b16f5
TootsieTall <75847495+TootsieTall@users.noreply.github.com>
2025-01-22 06:47:21 -0500

    Set up Flutter project structure

---
+++ HalalFinder_Flutter/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:halal_finder/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Halal Finder',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
